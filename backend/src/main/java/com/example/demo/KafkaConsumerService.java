e83b722562b6f606fa0f2efe2c6127285b8b3ecf
rainhyeon <iiilee0907@naver.com>
2025-04-26 13:26:47 +0000

    KafkaConsumerService log 수정

--- backend/src/main/java/com/example/demo/KafkaConsumerService.java
+++ backend/src/main/java/com/example/demo/KafkaConsumerService.java
@@ -14,15 +14,14 @@ public class KafkaConsumerService {
     public void listen(String message) {
         try {
             // 5. Kafka broker → Backend Consumer 수신
-            logger.info("[KafkaConsumerService] [4] [수신] Kafka Broker → Backend Consumer | payload='{}'", message);
+            logger.info("[KafkaConsumerService] [5] [수신] Kafka Broker → Backend Consumer | payload='{}'", message);
 
             // 6. WebSocket 브로드캐스트
             ChatWebSocketHandler.broadcast(message);
 
-            logger.info("[KafkaConsumerService] [5] [브로드캐스트] Backend Consumer → WebSocket Sessions | payload='{}'", message);
+            logger.info("[KafkaConsumerService] [6] [브로드캐스트] Backend Consumer → WebSocket Sessions | payload='{}'", message);
         } catch (Exception e) {
             logger.error("[KafkaConsumerService] [Error] Kafka 수신 또는 브로드캐스트 실패 | message='{}' | error='{}'", message, e.getMessage(), e);
         }
     }
 }
-s
